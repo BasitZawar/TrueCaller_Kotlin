16d60e538b82ba14db60fec9d1775b055eeb3b2d
basitzawar <basitzawar@gmail.com>
2022-07-26 16:54:44 +0500

    work on favorites contacts

--- app/src/main/java/com/lads/truecaller/Fragments/KeypadFragment.kt
+++ app/src/main/java/com/lads/truecaller/Fragments/KeypadFragment.kt
@@ -8,12 +8,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import com.lads.truecaller.R
 
 class KeypadFragment : Fragment() {
     lateinit var tv_Calling_Number: TextView
     lateinit var btnDail: ImageButton
+    lateinit var btnClear: ImageButton
     lateinit var textView1: TextView
     lateinit var textView2: TextView
     lateinit var textView3: TextView
@@ -32,6 +34,7 @@ class KeypadFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_keypad, container, false)
         btnDail = view.findViewById(R.id.btnDial)
+        btnClear = view.findViewById(R.id.btnClear)
         textView1 = view.findViewById(R.id.tv_1)
         textView2 = view.findViewById(R.id.tv_2)
         textView3 = view.findViewById(R.id.tv_3)
@@ -50,56 +53,58 @@ class KeypadFragment : Fragment() {
             val dailIntent = Intent(Intent.ACTION_CALL)
 //            dailIntent.data = Uri.parse("tel:" + "8344814819")
             dailIntent.data = Uri.parse("tel:" + tv_Calling_Number.text.toString())
-            getActivity()?.startActivity(dailIntent)
-//            val intent = Intent(
-//                requireContext(), CallingActivtiy::class.java
-//            )
-//            startActivity(intent)
+            activity?.startActivity(dailIntent)
         })
-
+        btnClear.setOnClickListener {
+            tv_Calling_Number.text = ""
+            btnClear.isVisible = false
+        }
         textView1.setOnClickListener(View.OnClickListener {
-            updateText(textView1.getTag().toString())
+            updateText(textView1.tag.toString())
         })
         textView2.setOnClickListener(View.OnClickListener {
-            updateText(textView2.getTag().toString())
+            updateText(textView2.tag.toString())
         })
 
         textView3.setOnClickListener(View.OnClickListener {
-            updateText(textView3.getTag().toString())
+            updateText(textView3.tag.toString())
         })
         textView4.setOnClickListener(View.OnClickListener {
-            updateText(textView4.getTag().toString())
+            updateText(textView4.tag.toString())
         })
         textView5.setOnClickListener(View.OnClickListener {
-            updateText(textView5.getTag().toString())
+            updateText(textView5.tag.toString())
         })
         textView6.setOnClickListener(View.OnClickListener {
-            updateText(textView6.getTag().toString())
+            updateText(textView6.tag.toString())
         })
         textView7.setOnClickListener(View.OnClickListener {
-            updateText(textView7.getTag().toString())
+            updateText(textView7.tag.toString())
         })
         textView8.setOnClickListener(View.OnClickListener {
-            updateText(textView8.getTag().toString())
+            updateText(textView8.tag.toString())
         })
         textView9.setOnClickListener(View.OnClickListener {
-            updateText(textView9.getTag().toString())
+            updateText(textView9.tag.toString())
         })
         textView0.setOnClickListener(View.OnClickListener {
-            updateText(textView0.getTag().toString())
+            updateText(textView0.tag.toString())
         })
         textViewStar.setOnClickListener(View.OnClickListener {
-            updateText(textViewStar.getTag().toString())
+            updateText(textViewStar.tag.toString())
         })
         textViewHash.setOnClickListener(View.OnClickListener {
-            updateText(textViewHash.getTag().toString())
+            updateText(textViewHash.tag.toString())
 
         })
+
         return view
 
     }
 
     fun updateText(newValue: String) {
-        tv_Calling_Number.setText("" + tv_Calling_Number.text + newValue)
+        tv_Calling_Number.text = "" + tv_Calling_Number.text + newValue
+        btnClear.isVisible = true
     }
+
 }
